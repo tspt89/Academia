48f83a99eba9b63d866972075930e24e16e9e625
tspt89 <tspt89@hotmail.com>
2021-05-26 04:13:32 -0500

    Pantalla principal del administrador con nav component

---
+++ app/src/main/java/com/tspt/academia/Administradores/HomeAdminFragment.kt
@@ -0,0 +1,13 @@
+package com.tspt.academia.Administradores
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.tspt.academia.R
+
+class HomeAdminFragment: Fragment(R.layout.fragment_home_admin) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
